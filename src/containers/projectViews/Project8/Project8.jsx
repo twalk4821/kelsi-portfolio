2e268fd880032777b09cc084cca2e167be88ee14
twalk4821 <tyjohnwalker@gmail.com>
2018-01-29 22:04:07 -0800

    new business card project

--- src/containers/projectViews/Project8/Project8.jsx
+++ src/containers/projectViews/Project8/Project8.jsx
@@ -10,6 +10,15 @@ export default (props) => (
     <div className="project-grid">
       <div className="item-1 content"></div>
       <div className="header"></div>
+      <div className="item-2 content"></div>
+      <div className="item-3 content"></div>
+      <div className="item-4 content"></div>
+      <div className="item-5 content"></div>
+      <div className="item-6 content"></div>
+      <div className="item-7 content"></div>
+      <div className="item-8 content"></div>
+      <div className="item-9 content"></div>
+      <div className="header"></div>
     </div>
     <div className="divider"></div>
     <div className="description">
